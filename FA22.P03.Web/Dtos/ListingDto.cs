ec02303ef9787e25b6df94d5a06642a70f7e2c24
Cameron Bonura <cameron.bonura@selu.edu>
2022-09-05 22:04:01 -0500

    Dtos are set up

--- FA22.P03.Web/Dtos/ListingDto.cs
+++ FA22.P03.Web/Dtos/ListingDto.cs
@@ -1,6 +1,16 @@
-﻿namespace FA22.P03.Web.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FA22.P03.Web.Dtos
 {
     public class ListingDto
     {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
     }
 }
--- FA22.P03.Web/Dtos/ProductDto.cs
+++ FA22.P03.Web/Dtos/ProductDto.cs
@@ -1,6 +1,14 @@
-﻿namespace FA22.P03.Web.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FA22.P03.Web.Dtos
 {
     public class ProductDto
     {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
     }
 }
