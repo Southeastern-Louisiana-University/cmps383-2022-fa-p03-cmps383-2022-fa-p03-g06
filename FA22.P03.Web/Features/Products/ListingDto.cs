16d27a88e464a399f40549cb45620a1095234cec
Cameron Bonura <cameron.bonura@selu.edu>
2022-09-05 22:33:42 -0500

    Moved things

---
+++ FA22.P03.Web/Features/Products/ListingDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FA22.P03.Web.Dtos
+{
+    public class ListingDto
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
